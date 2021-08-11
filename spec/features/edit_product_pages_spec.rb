e66f28ee6f45f0a57cf0e2242fea67e7c623bacb
Gengur123 <meleearthur@gmail.com>
2021-08-10 23:10:38 -0700

    Add intergration test for editing a product!

---
+++ spec/features/edit_product_pages_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "the edit a product process" do
+  it "edits a product" do
+    test_product = Product.create({:name => "Car", :cost => "10000", :country_of_origin => "Japan"})
+    visit products_path
+    click_on ("Car")
+    click_on 'Edit'
+    fill_in "Name", :with => "Wagon"
+    fill_in "Cost", :with => "1"
+    fill_in "product[country_of_origin]", :with => "1969"
+    click_on 'Update Product'
+    expect(page).to have_content "Wagon"
+  end
+end
