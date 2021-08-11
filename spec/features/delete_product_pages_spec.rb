faa53df422faade6b736efe1db733f555d6dbadd
Gengur123 <meleearthur@gmail.com>
2021-08-10 23:06:38 -0700

    Add integeration tests for adding a review and delete a product

---
+++ spec/features/delete_product_pages_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe "the delete a product process" do
+  it "deletes a new product" do
+    test_product = Product.create({:name => "Car", :cost => "1000", :country_of_origin => "Japan"})
+    visit products_path
+    click_on ("Car")
+    click_on 'Delete'
+    expect(page).to_not have_content "Car"
+  end
+
+end
