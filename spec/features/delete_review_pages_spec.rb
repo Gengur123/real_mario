e613a18d1e72563a99abe8ab16fd6632a274135c
Gengur123 <meleearthur@gmail.com>
2021-08-10 23:20:20 -0700

    Add intergration tests for deleting reivews and editing reivews IM DONE WITH IT WOOOOO

---
+++ spec/features/delete_review_pages_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "the delete review process" do
+it "deletes a review" do
+  test_product = Product.new({:name => "Car", :country_of_origin => "Japan", :cost => 10000})
+  test_product.save
+  test_review = Review.new({:author => "Bob", :rating => 5, :content_body => "This car go very fast vrooom vroom vrromm vrrommmm skrrttt", :product_id => test_product.id})
+  test_review.save
+  visit product_review_path(test_product, test_review)
+  click_on 'Delete'
+  expect(page).to_not have_content 'Bob'
+  end
+end
