51e792b7acdc79d039b1ac5bfde3f4b6ab34893c
Gengur123 <meleearthur@gmail.com>
2021-08-12 23:02:43 -0700

    ADD SCOPES AND ITS ALL FUNCTIONAL

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,13 @@
 
 class HomesController < ApplicationController
+
+  def index
+    @most_reviewed = Product.most_reviews
+    @recent = Product.most_recent
+    @usa = Product.made_in_usa
+    render :index
+  end
+  
   def show
     render :show
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,17 @@ class Product < ApplicationRecord
   validates :cost, presence: true
   validates :country_of_origin, presence: true
 
+  scope :most_recent, -> { order(created_at: :desc).limit(3)}
+  scope :made_in_usa, -> { where(country_of_origin: "USA")}
+
+  scope :most_reviews, -> {(
+    select("products.id, products.name, products.cost, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(1)
+    )}
+
   private
   def titleize_product
     self.name = self.name.titleize
