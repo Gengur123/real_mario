22455233c4b94bd917cc57f62b41d38f35ed4daf
Gengur123 <meleearthur@gmail.com>
2021-08-13 08:59:53 -0700

    Add users ability to sign up and sign in. Updated controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,15 @@ Rails.application.routes.draw do
   root to: 'homes#index'
 
   get '/homes' => 'homes#index'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
+
+
   resources :products do
     resources :reviews
   end
