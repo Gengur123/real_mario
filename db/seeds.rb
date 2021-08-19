50478292b8f6550a88e0091602ba50ecca15b042
Gengur123 <meleearthur@gmail.com>
2021-08-18 23:16:37 -0700

    Change the seeding for the reivews page

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ Review.destroy_all
                             cost: Faker::Number.between(from: 1, to: 10000),
                             country_of_origin: Faker::Address.country)
   
-  250.times do |review_instance|
+  5.times do |review_instance|
     Review.create!(author: Faker::Hipster.word,
                   content_body: 
                   Faker::Lorem.paragraph_by_chars(number: 250, supplemental: false),
